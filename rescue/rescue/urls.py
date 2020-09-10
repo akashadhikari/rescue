fc2435def769e5e193d8599ac31745af1ebcb99e
Akash Adhikari <akashsky1313@gmail.com>
2020-09-10 12:06:13 +0545

    Add home path slash (/)

--- rescue/rescue/urls.py
+++ rescue/rescue/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', include('connect.urls')),
+    path('/', include('connect.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
